2ba80078c5c44320ef9a9a059a9fd9d6cd2e1ab7
Abhinav Chandel <abnvchndel@gmail.com>
2025-10-07 00:55:11 +0530

    Adding files to the project

--- src/main/java/com/bankManagement/BankManagement/FleetHub.java
+++ src/main/java/com/bankManagement/BankManagement/FleetHub.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankManagementApplication {
+public class FleetHub {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankManagementApplication.class, args);
+		SpringApplication.run(FleetHub.class, args);
 	}
 
 }
